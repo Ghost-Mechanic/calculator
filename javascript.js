363c446492136c3119e2a94ba681db4fba42f540
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-06-04 00:57:22 -0500

    Created four functions for basic operations: adding, subtracting, multiplying, and dividing

---
+++ javascript.js
@@ -0,0 +1,26 @@
+/***********************************************************
+This is the script for a calculator on a webpage 
+************************************************************/
+
+
+// four functions for four basic operators
+function add(leftNum, rightNum) {
+    return leftNum + rightNum;
+}
+
+function subtract(leftNum, rightNum) {
+    return leftNum - rightNum;
+}
+
+function multiply(leftNum, rightNum) {
+    return leftNum * rightNum;
+}
+
+function divide(leftNum, rightNum) {
+    return leftNum / rightNum;
+}
+
+// declare three variables for three parts of math equation
+let leftNum;
+let operator;
+let rightNum;
