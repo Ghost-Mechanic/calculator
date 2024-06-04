30a0a284b6d0c67ec8b6b04a1e4555d5dfcfbc27
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-06-04 01:03:32 -0500

    created calculate() function that takes in two numbers and an operator and performs calculation based on operator

--- javascript.js
+++ javascript.js
@@ -20,6 +20,22 @@ function divide(leftNum, rightNum) {
     return leftNum / rightNum;
 }
 
+// this function takes in two numbers and an operator and performs the operation accordingly
+function calculate(leftNum, operator, rightNum) {
+    switch (operator) {
+        case "+": 
+            return add(leftNum, rightNum);
+        case "-":
+            return subtract(leftNum, rightNum);
+        case "*":
+            return multiply(leftNum, rightNum);
+        case "/":
+            return divide(leftNum, rightNum);
+        default:
+            return "Invalid operation";
+    }
+}
+
 // declare three variables for three parts of math equation
 let leftNum;
 let operator;
